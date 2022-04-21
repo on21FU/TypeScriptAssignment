ead09eaadbe145a957767411a369f3565b863cd5
on21FU <flo.uhlschmiedmx.de>
2022-04-21 12:11:23 +0200

    optimize formatePrice()

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -2,8 +2,7 @@ export const innerShopContainer = document.getElementById('inner_shop_container'
 export const cartButtons = document.getElementsByClassName('cartButton');
 
 export function formatePrice(price: number){
-    let priceWithComma = price.toString().replace(".", ",");
-    let priceSplit = priceWithComma.split(",");    
+    let priceSplit = price.toString().split(".");    
     if(priceSplit[1] != null && priceSplit[1].length == 1) {
         priceSplit[1] = priceSplit[1] + "0";
     }
