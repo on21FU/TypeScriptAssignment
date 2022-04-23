d1466d5dcdf9534a017c60ca2581a9dc932f9d24
on21FU <flo.uhlschmiedmx.de>
2022-04-23 16:35:10 +0200

    shopping cart css

--- src/shopping-cart.ts
+++ src/shopping-cart.ts
@@ -1,12 +1,12 @@
-import { body, darkBackground, shoppingCart } from "./dom-utils";
+import { noScrollContainer, darkBackground, shoppingCart } from "./dom-utils";
 
 export function showShoppingCart(){
     shoppingCart.style.right = "0";
     darkBackground.style.display = "block";
-    body.classList.add('stop-scrolling');
+    noScrollContainer.classList.add('stop-scrolling');
 }
 export function closeShoppingCart(){
     shoppingCart.style.right = "-600px";
     darkBackground.style.display = "none";
-    body.classList.remove('stop-scrolling');
+    noScrollContainer.classList.remove('stop-scrolling');
 }
