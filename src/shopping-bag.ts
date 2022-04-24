553593219b909893d4756933475a730cf659756a
on21FU <flo.uhlschmiedmx.de>
2022-04-24 18:16:32 +0200

    dynamic shopping cart articles

--- src/shopping-bag.ts
+++ src/shopping-bag.ts
@@ -1,7 +1,8 @@
 import { shoppingBagCointainer } from "./dom-utils";
 import { allArticles } from "./manage-data";
+import { formatePrice } from "./side-functions";
 
-let shoppingCart: Object[] = [];
+let shoppingCart: Array<any> = [];
 
 export function addToCart(event: any){
     let articleID = Number.parseInt(getArticleID(event));
@@ -25,5 +26,27 @@ function buildCartArticle(){
     if(shoppingBagCointainer.childNodes[1].nodeName === "P"){
         shoppingBagCointainer.childNodes[1].remove();
     }
+        const article = shoppingCart[shoppingCart.length-1];
+    
+        const newCartArticle = document.createElement('div');
+        newCartArticle.classList.add('cartArticle');
+        newCartArticle.innerHTML = `
+            <div class="cartArticle-img">
+            <img src="${article.image}">
+            </div>
+            <div class="cartArticle-content">
+                <p>${article.title}</p></br>
+                <p id="price">${formatePrice(article.price)} €</p>
+            </div>
+            <div class="cartArticle-removebtn">
+                <p>X</p>
+            </div>
+        `;
+        shoppingBagCointainer.appendChild(newCartArticle);
+
+        const divider = document.createElement('div');
+        divider.classList.add('divider');
+        shoppingBagCointainer.appendChild(divider);
+    
     
 }
